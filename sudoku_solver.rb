a6bd3b9b6086465674d632378f2c023144b466bf
Ricardo Rubio <rickarubio@gmail.com>
2013-07-23 04:34:09 -0600

    successfully implemented the create_sudoku_boxes method and added enhancements to the display method

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -13,14 +13,45 @@ class Sudoku
 
   def create_sudoku_boxes(sudoku_rows)
     # There are 9 boxes (3x3) from top left to right and continuing down
+    sudoku_groupings = [[], [], [], [], [], [], [], [], []]
     sudoku_boxes = [[], [], [], [], [], [], [], [], []]
-    for i in 0..2
+    (0..2).each_with_index do |obj, i|
       sudoku_rows.each_with_index do |row, index|
         slice_start = i * 3
-        sudoku_boxes[index].push row.slice(slice_start, 3)
+        sudoku_groupings[index].push row.slice(slice_start, 3)
       end
     end
-    sudoku_boxes.each {|box| print box.to_s + "\n"}
+    #sudoku_groupings.each {|box| print box.to_s + "\n"} # for testing
+
+    # The values obtained above must now be properly interpreted for
+    # the sudoku box objects (3x3 squares) top left is box1, bottom right box9
+    for box_index in 0..8
+      sudoku_groupings.each_with_index do |row, row_index|
+        # form box 0 (the top left corner sudoku box)
+        if box_index == 0 && row_index == 0 || box_index == 0 && row_index == 1 || box_index == 0 && row_index == 2
+          sudoku_boxes[box_index].push row[0]
+        elsif box_index == 1 && row_index == 0 || box_index == 1 && row_index == 1 || box_index == 1 && row_index == 2
+          sudoku_boxes[box_index].push row[1]
+        elsif box_index == 2 && row_index == 0 || box_index == 2 && row_index == 1 || box_index == 2 && row_index == 2
+          sudoku_boxes[box_index].push row[2]
+        elsif box_index == 3 && row_index == 3 || box_index == 3 && row_index == 4 || box_index == 3 && row_index == 5
+          sudoku_boxes[box_index].push row[0]
+        elsif box_index == 4 && row_index == 3 || box_index == 4 && row_index == 4 || box_index == 4 && row_index == 5
+          sudoku_boxes[box_index].push row[1]
+        elsif box_index == 5 && row_index == 3 || box_index == 5 && row_index == 4 || box_index == 5 && row_index == 5
+          sudoku_boxes[box_index].push row[2]
+        elsif box_index == 6 && row_index == 6 || box_index == 6 && row_index == 7 || box_index == 6 && row_index == 8
+          sudoku_boxes[box_index].push row[0]
+        elsif box_index == 7 && row_index == 6 || box_index == 7 && row_index == 7 || box_index == 7 && row_index == 8
+          sudoku_boxes[box_index].push row[1]
+        elsif box_index == 8 && row_index == 6 || box_index == 8 && row_index == 7 || box_index == 8 && row_index == 8
+          sudoku_boxes[box_index].push row[2]
+        end
+      end
+      box_index += 1
+    end
+    return sudoku_boxes
+    #sudoku_boxes.each_with_index {|box, index| print " box #{index}" + box.to_s + "\n"} # for testing
   end
 
   def display
@@ -33,6 +64,18 @@ class Sudoku
     puts "=" * @sudoku_columns[0].to_s.length
     @sudoku_columns.each {|col| print col.to_s + "\n"}
     puts "=" * @sudoku_columns[0].to_s.length
+    puts
+    puts "Your Sudoku Boxes by the Number:"
+    @sudoku_boxes.each_with_index {|box, index| print " box #{index}: " + box.to_s + "\n"}
+    print "box guide map:"
+    puts "
+          -------
+          |1|2|3|
+          |- - -|
+          |4|5|6|
+          |- - -|
+          |7|8|9|
+          -------"
   end
 end
 
@@ -47,4 +90,4 @@ sudoku_puzzle = Sudoku.new([
   [6, 0, 0, 3, 4, 0, 0, 0, 1],
   [3, 0, 8, 7, 1, 0, 0, 0, 4],
   [0, 1, 0, 0, 0, 0, 6, 0, 7]])
-#sudoku_puzzle.display
+sudoku_puzzle.display
