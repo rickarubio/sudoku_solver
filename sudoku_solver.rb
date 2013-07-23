801031f714a6cc88a6a93683c727d5ec0c8dd4d5
Ricardo Rubio <rickarubio@gmail.com>
2013-07-23 00:43:13 -0600

    add display method and begin work on finding the 9 3x3 sudoku boxes of a sudoku board

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,14 +1,35 @@
 # This program solves any sudoku puzzle
 # Ricardo A. Rubio
 # http://www.rickarubio.com
-class SudokuSolver
-  def initialize(sudoku_board)
-    @sudoku_rows = sudoku_board
-    @sudoku_columns = sudoku_board.transpose
+class Sudoku
+  def initialize(sudoku_rows)
+    # rows are ordered from top to bottom
+    @sudoku_rows = sudoku_rows
+    # columns are ordered from left to right
+    @sudoku_columns = sudoku_rows.transpose
+    # There are 9 boxes (3x3) from top left to right and continuing down
+    @sudoku_boxes = create_sudoku_boxes(@sudoku_rows)
+  end
+
+  def create_sudoku_boxes(sudoku_rows)
+    puts "created"
+  end
+
+  def display
+    puts "Your Sudoku Board (arranged by rows):"
+    puts "=" * @sudoku_rows[0].to_s.length
+    @sudoku_rows.each {|row| print row.to_s + "\n"}
+    puts "=" * @sudoku_rows[0].to_s.length
+    puts ""
+    puts "Your Sudoku Board (arranged by column values):"
+    puts "=" * @sudoku_columns[0].to_s.length
+    @sudoku_columns.each {|col| print col.to_s + "\n"}
+    puts "=" * @sudoku_columns[0].to_s.length
   end
 end
 
-sudoku_puzzle = SudokuSolver.new([
+# http://www.sudokukingdom.com/images/BlockTutorSolve.gif
+sudoku_puzzle = Sudoku.new([
   [0, 0, 9, 1, 0, 0, 0, 0, 2],
   [0, 4, 0, 8, 0, 5, 0, 0, 0],
   [0, 8, 0, 0, 0, 0, 0, 1, 5],
@@ -18,4 +39,4 @@ sudoku_puzzle = SudokuSolver.new([
   [6, 0, 0, 3, 4, 0, 0, 0, 1],
   [3, 0, 8, 7, 1, 0, 0, 0, 4],
   [0, 1, 0, 0, 0, 0, 6, 0, 7]])
-p sudoku_puzzle
+#sudoku_puzzle.display
